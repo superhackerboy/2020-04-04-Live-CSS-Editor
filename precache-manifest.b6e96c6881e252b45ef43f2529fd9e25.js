eefa3d753cc444e35446758612803fe9bb3d3f93
Mark Harless <mark@superhackerman.com>
2020-04-04 15:05:42 -0400

    Updates

---
+++ precache-manifest.b6e96c6881e252b45ef43f2529fd9e25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9f831436b4a6715ce7a245022e952c7",
+    "url": "/2020-04-04-Live-CSS-Editor/index.html"
+  },
+  {
+    "revision": "d16cb7669cfa93fcb4b2",
+    "url": "/2020-04-04-Live-CSS-Editor/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d2023940058c808dc5b3",
+    "url": "/2020-04-04-Live-CSS-Editor/static/js/2.a860f238.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/2020-04-04-Live-CSS-Editor/static/js/2.a860f238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d16cb7669cfa93fcb4b2",
+    "url": "/2020-04-04-Live-CSS-Editor/static/js/main.f785ad06.chunk.js"
+  },
+  {
+    "revision": "f8504c78fa88e3466421",
+    "url": "/2020-04-04-Live-CSS-Editor/static/js/runtime-main.a7def18e.js"
+  }
+]);
